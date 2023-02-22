8a4450d0d6bfb6efeec6ef68d7c6cd1c17127209
Anrishainidze <123399273+Anrishainidze@users.noreply.github.com>
2023-02-22 16:26:14 +0400

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+
+
+
+var income = prompt("შეიყვანე თქვენი ხელფასი")
+if (income < 300){
+    alert("საშუალოზე დაბალი")
+}
+else if (income >= 300 && income <500){
+    alert("sashualo xelfasia")
+}
+else if (income >=500 && income < 2500){
+    alert("kargi xelfasia")
+}
+else{
+    alert("kargi xar jigaro")
+}
+
+
+
+// console.log(people)
+
+//     const arr = [1,4,18,24,15,2,3,19,55]
+//     const=even
